cd09b787a246793abdbf5ab73166841118571b00
smith-villavicencio <57007707+smith-villavicencio@users.noreply.github.com>
2019-10-25 09:30:19 -0500

    trabajo de programación 3

---
+++ villavicencio1.py
@@ -0,0 +1,266 @@
+nombre="smith villavicencio"
+edad=18
+DNI=71091007
+fecha_de_solicitud="10/09/19"
+modalidad_de_afilación="individual"
+plan_de_beneficios="peas"
+aporte_mensual=39
+DNI=71091007
+apellido="villavicencio sanchez"
+nombre="nelson smith"
+situacion_del_titular="asegurado"
+fecha_de_nacimiento="30/05/2001"
+celular=950412307
+sexo="masculino"
+correo_electronico="nesonsmithv@gmail.com"
+departamento="amazonas"
+provincia="utcubamba"
+distrito="bagua grande"
+direccion="jr cristobal colon piso 01"
+facultad="FACFyM"
+escuela_profesional="ingenieria electronica"
+departamento_academico="de matematica"
+curso="analisis matematico 1"
+codigo_del_curso="MM 100"
+ciclo_de_estudios="primer ciclo"
+semestre_academico="2019-II"
+pre_requisito="ninguno"
+creditos="04 creditos"
+fecha_de_inicio="16/09/2019"
+fecha_de_termino="09/01/2020"
+horas_semanales="06 horas"
+horas_totales="96 horas academicas"
+docente="dr. andres figueroa alvarado"
+e_mail="figueal@gmail.com"
+e_mail_institucional="afigueroa@unprg.edu.pe"
+semana="siete dias"
+dia="24 horas"
+hora="60 minutos"
+minuto="60 segundos"
+anio_gregoriano="365 dias"
+lustro="5 años"
+decada="10 años"
+siglo="100 años"
+pulgada="2.54 cm"
+metro="100 cm"
+pie="30.48 cm"
+yarda="36 plg"
+milla_terrestre="1609 m"
+milla_maritima="1852 m"
+kilometro="1000 m"
+BTU="200 cal"
+pto_fusion_kelvin=273
+pto_fusion_celcius=0
+pto_fusion_rankine=492
+pto_fusion_fahrenheit=32
+pto_ebullicion_kelvin=373
+pto_ebullicion_celcius=273
+pto_ebullicion_rankine=672
+pto_ebullicion_fahrenheit=212
+simbolo="cu"
+capacidad_calorifica=24.4
+calor_especifico="0.39 j/gk"
+entalpia_de_formacion="298.15 k"
+conductividad_termica="298 k"
+energia_de_enlace="176 kj/mol"
+pto_ebullicion="2562 ºc"
+pto_fusion="1084 ºc"
+calor_vaporizacion="304.3 kj/mol"
+electronegatividad="1.90 pauling"
+calor_fusion="12.93kj/mol"
+electroafinidad="119kj/mol"
+radio_covalente_pm=117
+numero_atomico=29
+densidad="8.93g/cm`3"
+estructura_electronica="3d`10 4s`1"
+radio_atomico_pm=128
+radio_ionico_pm="90(+1) 72(+2)"
+peso_atomico=63.55
+volumen_atomico="7.09cm`3/mol"
+primera_energia="749kj/mol"
+calor_latente_de_fusion="203j/g"
+tipo_de_letra="times new roman"
+tamaño_de_letra=12
+interlineado=2
+margen_superior=2.54
+margen_inferior=2.54
+margen_derecha=2.54
+margen_izquierdo=2.54
+numero_de_paginas="superior derecha"
+sangria="media pulgada"
+perimetro_abdominal="90/50 mmhg"
+frecuencia_cardiaca="72imp"
+frecuencia_respiratoria="20imp"
+saturacion_de_oxigeno="98%"
+peso="71kg"
+talla="1.69 cm"
+apellido_paterno="ovando"
+apellido_materno="molina"
+nombre="victor hugo"
+domicilio="calle encino 09"
+colonia="juan n alvarez"
+codigo_postal=39030
+ciudad="chilpancingo"
+lugar_de_nacimiento="chilpancingo de los bravos"
+sexo="masculino"
+nombre_del_padre="ovando leyva regulo"
+ocupacion_del_padre="ingeniero agronomo"
+nacionalidad_del_padre="españa"
+nombre_de_la_madre="molina sanchez cecilia"
+ocupacion_de_la_madre="ama de casa"
+nacionalidad_de_la_madre="españa"
+puesto_que_solicita="responsabe administrativo"
+reg_de_contribuyente="oamv810521g84"
+clave_unica_de_poblacion="oamv810521hgrvlc03"
+tiene_licencia_de_manejo="si"
+numero_de_licencia="oc-00009431"
+numero_de_cartilla_militar="c-5608122"
+estado_de_salud="bueno"
+deporte="atletismo"
+padece_enfermedad="no"
+pasatiempo="internet"
+primaria="primer congreso de anahuac"
+secundaria="raymudo abarca alarcon"
+preparatoria="aaron m.flores"
+profesional="unidad academica de ingenieria"
+maestria="colegio mayor de san carlos"
+titulo_de_primaria="certificado"
+titulo_de_secundaria="certificado"
+titulo_de_preparatoria="certificado"
+titulo_de_profesional="titulo y cedula"
+titulo_de_maestria="titulo y cedula"
+edad="33 años"
+ensamblado="china"
+ac="5nlhj-088kft-nf3f6"
+wo=8252530
+id=10387
+marca="kington"
+descripcion=3.0
+sirena="wiu wiu wiuuu"
+claxon="tiii tiii tiii"
+silbato="tfhiii thifiiiii"
+reloj="tic tac tic tac"
+silbar="fhifhifhiiii"
+microfono="hola, si"
+pelotaso="pummmm"
+prender_laptop="wihun"
+viento="fuuuffff"
+timbre="tiririririri"
+explosion="pooommmmm"
+disparo="pum"
+desbloqueo_del_celular="trii"
+clavar="tum tum tum"
+alarma="lililili"
+moto="rum rum rum"
+captura_de_pantalla="trush"
+tomar_foto="crosh"
+desllavar_puerta="tla tla tla"
+deshojar="fhes fhes"
+estornudo="achis"
+hervir_agua="fhufhufhuuu"
+accidente="pooom"
+campanasos="pum pum pum"
+licuar="uhh uhhhhh"
+clip="cli"
+perforar="clap"
+engrapar="crup"
+reventar_globo="puummmm"
+inflar_globo="fhuumm fooomm"
+gritar="ahahhhh aaahhhh"
+barrer="suu su shuu"
+llorar="mm mmmm mmmm"
+soldar="ruuumm ruuumm"
+celular="tululu"
+teclado="tun tun tun"
+galleta="chashshsh"
+tocar_puerta="pom pom pom"
+ducharse="shi shixs shi"
+cortar_madera="sh shu shush"
+hueco_a_una_madera="juuu juu juu"
+bombo="puum pum"
+positivo_con_negativo="thish thish"
+toser="jacm jacm"
+imprimir="fuc fuc fuc"
+disparar="ra ta ta ra ta"
+relinchar="wiii wiii"
+zumba="bzzz bzz"
+rebusnar="hiaaa hiaaa"
+chilar="u u aa uu"
+chasquido="iii iii"
+canta="kikiriki"
+grunido="oink oink"
+chillido="iii ii"
+balido="bee beee"
+chirrido="cri cri cri"
+parpa="cuac cuac"
+murgido="muuu muu"
+grazna="pio pio"
+ladrar="guau guau"
+maulla="miau miau"
+rugir="grrr grr"
+print("Mi nombre es:",nombre , "Mi edad:", edad ,"Mi nº de dni es:", DNI)
+print("Mi fecha de solicitud es:",fecha_de_solicitud ,"Mi modalidad de afilación es:" , modalidad_de_afilación , " Mi plan de beneficios es:" , plan_de_beneficios)
+print("El aporte mensual es:",aporte_mensual ,"Mi nº de dni es:", DNI ,"Mi apellido es:", apellido)
+print("Mi nombre es:",nombre,"Mi situacion titular es:", situacion_del_titular ,"Mi fecha de nacimiento es:", fecha_de_nacimiento)
+print("Mi celular es:",celular,"Mi sexo es:",sexo,"Mi correo electronico es:",correo_electronico)
+print("Mi departamento es:",departamento, "Mi provincia es:",provincia, "Mi distrito es:",distrito)
+print("Mi direccion es:",direccion, "Mi facultad es:",facultad, "Mi escuela profesional es:", escuela_profesional)
+print("El departamento academico es:",departamento_academico, "El curso es:",curso, "El codigo del curso es:",codigo_del_curso)
+print("El ciclo de estudios es:",ciclo_de_estudios, "El semestre academico es:",semestre_academico, "El pre requisito es:",pre_requisito)
+print("El nº de creditos es:",creditos, "La fecha de inicio es:",fecha_de_inicio, "La fecha de termuno es:",fecha_de_termino)
+print("El nº de horas semanales es:",horas_semanales, "El nº de horas totales es:",horas_totales, "El docente es:",docente)
+print("El e-mail es:",e_mail, "El e-mail institucional es:",e_mail_institucional, "La semana tiene:",semana)
+print("El dia tiene:",dia, "La hora tiene:",hora, "Un minuto tiene:",minuto)
+print("Un anio gregoriano tiene:",anio_gregoriano, "Un lustro tiene:",lustro, "Una decada tiene:",decada)
+print("Un siglo consta de:",siglo, "Una pulgada en cm mide:",pulgada, "Un metro en cm mide:",metro)
+print("Un pie en cm mide:",pie, "Una yarda en plg mide:",yarda, "Una milla terrestre en m mide:",milla_terrestre)
+print("Una milla maritima en m mide:",milla_maritima, "Un kilometro consta de:",kilometro, "Un BTU es:",BTU)
+print("El pto de fusion kelvin equivale:",pto_fusion_kelvin, "El pto de fusion celcius equivale:",pto_fusion_celcius, "El pto de fusion rankine equivale:",pto_fusion_rankine)
+print("El pto fusion fahrenheit equivale:",pto_fusion_fahrenheit, "El pto ebullicion kelvin equivale:",pto_ebullicion_kelvin, "El pto de ebullicion celcius equivale:",pto_ebullicion_celcius)
+print("El pto ebullicion rankine equivale:",pto_ebullicion_rankine, "El pto ebulicion fahrenheit equivale:",pto_ebullicion_fahrenheit, "El simbolo del cobre es:",simbolo)
+print("La capacidad calorifica del cu es:",capacidad_calorifica, "El calor especifico del cu es:",calor_especifico, "la entalpia de formacion es:",entalpia_de_formacion)
+print("La conductividad termica del cu es:",conductividad_termica, "La energia de enlace del cu es:",energia_de_enlace, "El pto ebullicion del cu es:",pto_ebullicion)
+print("El pto fusion del cu es:",pto_fusion, "El calor de vaporizacion:",calor_vaporizacion, "La electronegatividad del cu es:",electronegatividad)
+print("El calor fusion del cu es:",calor_fusion, "La electroafinidad del cu es:",electroafinidad, "El radio covalente del cu es:",radio_covalente_pm)
+print("El nº atomico del cu es:",numero_atomico, "La densidad del cu es:",densidad, "Su estructura electronica es:",estructura_electronica)
+print("El radio atomico del cu es:",radio_atomico_pm, "El radio ionico del cu es:",radio_ionico_pm, "El peso atomico del cu es:",peso_atomico)
+print("El volumen atomico del cu equivale:",volumen_atomico, "Su primera energia del cu es:",primera_energia, "El calor latente de fusion del cu es:",calor_latente_de_fusion)
+print("El tipo de letra segun normas apa es:",tipo_de_letra, "El tamaño es:",tamaño_de_letra, "Su interlineado es:",interlineado)
+print("El margen superior segun normas apa mide:",margen_superior, "Su margen inferior mide:",margen_inferior, "Su margen derecho mide:",margen_derecha)
+print("El margen izquierdo segun normas apa mide:",margen_izquierdo, "El lugar de nº de paginas se ubica:",numero_de_paginas, "Su sangria mide:",sangria)
+print("Mi perimetro abdominal es:",perimetro_abdominal, "Mi frecuencia cardiaca es:",frecuencia_cardiaca, "Mi frecuencia respiratoria es:",frecuencia_respiratoria)
+print("Mi saturacion de oxigeno es:",saturacion_de_oxigeno, "Mi peso es:",peso, "Mi talla es:",talla)
+print("Su apellido paterno es:",apellido_paterno, "Su apellido materno es:",apellido_materno, "Su nombre es:",nombre)
+print("Su domicilio esta ubicado en:",domicilio, "Su colonia se encuentra en:",colonia, "Su codigo postal es:",codigo_postal)
+print("Su ciudad es:",ciudad, "Su lugar de naciemnto es:",lugar_de_nacimiento, "Su sexo es:",sexo)
+print("El nombre de su padre es:",nombre_del_padre, "La ocupacion de su padre es:",ocupacion_del_padre, "La nacionalidad de su padre es:",nacionalidad_del_padre)
+print("El nombre de su madre es:",nombre_de_la_madre, "La ocupacion de su madre es:",ocupacion_de_la_madre, "La nacionalidad de su madre es:",nacionalidad_de_la_madre)
+print("El puesto que solicita es:",puesto_que_solicita, "Su reg de contribuyente es:",reg_de_contribuyente, "Su clave unica de la poblacion es:",clave_unica_de_poblacion)
+print("Esta autorizado para que pueda conducir:",tiene_licencia_de_manejo, "su numero de licencia es:",numero_de_licencia, "su numero de cartilla militar es:",numero_de_cartilla_militar)
+print("Su estado de salud es:",estado_de_salud, "el deporte que practica es:",deporte, "padece de elguna enefermedad:",padece_enfermedad)
+print("Su pasatiempo favorito es:",pasatiempo, "Sus estudios primarios los realizo en:",primaria, "sus estudios secundarios en:",secundaria)
+print("Su preparatorio lo realizo en:",preparatoria, "su carrera profesional en:",profesional, "su maestria lo dio en:",maestria)
+print("El titulo recibido de primaria es:",titulo_de_primaria, "El titulo recibido de secundario es:",titulo_de_secundaria, "su titulo de preparatoria es:",titulo_de_preparatoria)
+print("El titulo recibido de profesional es:",titulo_de_profesional, "el titulo de maestria es:",titulo_de_maestria, "su edad es:",edad)
+print("El ensamblado se da en",ensamblado, "el ac del usb es:",ac, "el wo del usb es:",wo)
+print("El id del usb es:",id, "La marca es:",marca, "Su descripcion es:",descripcion)
+print("El ruido de una sirena es:",sirena, "El ruido de un claxon suena:",claxon, "El ruido de un silbato suena:",silbato)
+print("El sonido de un reloj es:",reloj, "El sonido al silbar es:",silbar, "Al habalr por microfono una de las palabras que se escucha es:",microfono)
+print("El sonido al dar un pelotaso suena:",pelotaso, "El sonido al prender una laptop:",prender_laptop, "El viento suena:",viento)
+print("Al tocar un timbre suena:",timbre, "Una explosion suena:",explosion, "Un disparo suena:",disparo)
+print("Al desbloquear un celular suena:",desbloqueo_del_celular, "Al clavar suena:",clavar, "la alarma de un despertador suena:",alarma)
+print("Una moto cuando esta encendida suena:",moto, "Al capturar una pantalla desde mi celular suena:",captura_de_pantalla, "Al tomar foto:",tomar_foto)
+print("Al desllavar una puerta se escucha:",desllavar_puerta, "Al deshojar suena:",deshojar, "Al estornudar se escucha:",estornudo)
+print("Al hervir agua suena:",hervir_agua, "un accidente entre automoviles suena:",accidente, "los campanasos suenan:",campanasos)
+print("Al licuar se escucha un ruido asi:",licuar, "al hacer un clip con un mouse suena:",clip, "al perforar suena:",perforar)
+print("Al engrapar se escuha:",engrapar, "al reventar un globo:",reventar_globo, "al inflar un globo:",inflar_globo)
+print("Al gritar una persona se escucha:",gritar, "al barrer se escucha:",barrer, "al llorar se escuha:",llorar)
+print("Al soldar se escucha:",soldar, "al encender un celular este suena:",celular, "al tipear en teclado se escucha:",teclado)
+print("Al abrir la envoltura de una galleta este se escucha:",galleta, "al tocar una puerta suena:",tocar_puerta, "al ducharse se escuha:",ducharse)
+print("Al cortar la madera con la amoladora este emite un ruido asi:",cortar_madera, "al hacer hueco la madera con el talado se escucha:",hueco_a_una_madera, "al golpear un bombo suena:",bombo )
+print("Al tocar dos cables electricos un positivo y el otro negativo estos suenan:",positivo_con_negativo, "al toser se escucha:",toser, "al sacar una impresion se escucha:",imprimir)
+print("Al disparar con una ametralladora se escuha:",disparar, "al relinchar un caballo lo hace:",relinchar, "al zumbar una abeja lo hace:",zumba)
+print("Al rebusnar un burro se escucha:",rebusnar, "a chilar un mono se escucha:",chilar, "el chasquido de un delfin se escucha:",chasquido)
+print("Al cantar un gallo se escucha:",canta, "al gruñir un cerdo se escucha:",grunido, "al chillar un raton se escucha:",chillido)
+print("Al balir una oveja se escucha:",balido, "el chirrido de un grillo se escucha:",chirrido, "al parpo de un pato se escucha:",parpa)
+print("El murgido de una vaca se escucha:",murgido, "al graznar un ave se escucha:",grazna, "el perro al ladrar se escucha:",ladrar)
