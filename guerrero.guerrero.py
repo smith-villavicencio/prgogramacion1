85690e924833b8b10ef910b7e3876418760a986d
smith-villavicencio <57007707+smith-villavicencio@users.noreply.github.com>
2019-10-25 19:17:26 -0500

    trabajo # 3

---
+++ guerrero.guerrero.py
@@ -0,0 +1,264 @@
+nombre_del_alumno="roberto guerrero"
+numero_dni_del_alumno="7555780"
+direccion="fanny abanto mz.n"
+fecha_de_deposito="12/10/2019"
+n_de_operacion="528725"
+hora_de_operacion="16:00:11"
+agente_bcp="farmancia ABC"
+nombre="izquierdo serrrato"
+monto_recibido="S/.50.00"
+categoria_vehicular="dos b"
+clase_vehicular="A"
+n_de_licencia="c45353098"
+fecha_de_expedicion="29/03/2002"
+fecha_de_revalidacion="05/08/2018"
+fecha_de_nacimiento_del_condcutor="12/09/1988"
+grupo_sanguinio="O+"
+donate_de_organos="false"
+n_primigenio="45353098"
+sery="2"
+ruc="1016650121"
+cliente="rosa"
+nombre_de_la_madre="rosa guerrero"
+n_de_boleta="37038"
+importe="S/.25.00"
+nombre_del_producto="guarda polvo"
+direccion_de_la_tienda="vicente de la vega #633"
+propietario="tarrillo flor maria"
+n_de_recibo="251-38839838"
+codigo_de_consultas="225534955"
+tipo_de_conexion="monofasica"
+consumo="18.33 kwh"
+potencia_contratada="1.00 kw"
+inicio_de_contrato="31/10/2008"
+termino_de_contrato="30/10/209"
+fecha_de_vencimiento="11/10/2019"
+descuento_fise="s/.16.00"
+total_a_pagar="S/.15.00"
+IGV="S/.0.84"
+alumbrado_publico="S/.0.42"
+pagina_web="www.distriluz.com.pe"
+DNI_del_cliente="16463165"
+nombre_de_la_empresa="Ensa"
+nombre_del_usuario="rosa guerrero guerrero"
+modulo="15"
+direccion_y_sentido="primer cuadrante"
+temperatura="18°c"
+autor_de_introducion_al_analisis_matematico="a.venero"
+codigo_de_estudiante="14321d"
+importe_de_matricula="S/.270"
+total="S/.280"
+pagina_web_de_la_unprg="www.unprg.edu.pe"
+n_telefonico="955432963"
+imei="12136168910213"
+fecha_de_lanzamiento="2015"
+emoji="☺"
+nombre_de_la_marca="adidas"
+correo_electronico="robertoguerrero20001@gmail.com"
+contrasena="1234unprg"
+altura="1.85m"
+peso="75.8kg"
+gravedad_de_la_tierra="9.8m/s"
+formula_de_la_energia="e=mc"
+autor_de_la_teoria_de_la_relatividad="alberth einsten"
+sistema_operativo="android9.0"
+sistema_operativo_de_la_computadora="windows7 ultimate"
+nombre_del_videojuego="lol"
+animal="gato"
+codigo_postal="21340"
+pi="3.14"
+cancion="fantasias"
+E="2.71"
+heroe_lambayecano="jose abelardo quiñones"
+lider_del_grupo_terrorista_sendero_luminoso="abimael Guzman"
+edad=19
+fecha_de_inscripsion="13/08/2000"
+promedio_final="14"
+n_de_ruedas="4"
+n_de_placa="3145-d"
+serie_de_chasis="13561h"
+color_del_vehiculo="rojo"
+modelo="l455"
+lugar_de_ocurrencia="piura"
+n_de_partida="15328"
+filial="chiclayo"
+vaucher_n="123591"
+agencia_de_pago="0233"
+tipo="nacional"
+departamento="lambayeque"
+provincia="chiclayo"
+A_egreso="2018"
+escuela="ingeneria electronica"
+turno="mañana"
+secuencia="92"
+ciclo="2019II"
+afiliado="sis"
+aula="11"
+local="cpu chiclayo"
+ley_n="26790"
+unidad="servicios"
+tension="220 v"
+hilos="2"
+distrito="pimentel"
+descripcion="shampoo herbal"
+precio="S/,10.50"
+descuento="S/.3.50"
+ugel="ugel chiclayo"
+nivel="secundaria"
+colegio_egresado="karl weiss"
+grado="quinto"
+seccion="E"
+sexo="masculino"
+pago_del_mes="S/.203"
+intereses="s/.50.00"
+ubicacion_de_peru="america del sur"
+pais="peru"
+n_de_tarjeta="4040 7100 0082 7442"
+resolucion="042-2013-A-MDCE"
+area_de_terreno="140.00 M2"
+forma_de_pago="efectivo"
+servicios="agua y desague"
+primer_premio="un pavo"
+lengua="castellano"
+religion="catolico"
+estado_civil:"soltero"
+n_hermanos="2"
+color_de_ojos="negro"
+tienda="inkafarma"
+discapacidad="false"
+tipo_de_documeto="dni"
+tipo_de_moneda="sol"
+ocupacion="ama de casa"
+pagina_web_del_banco_de_la_nacion="www.bn.com.pe"
+nombre_de_la_universidad="unprg"
+nombre_del_curso="programacion 1"
+nombre_del_docente="fernando diaz sanchez"
+operadora="movistar"
+tipo_de_archivo="zip"
+plan_contratado="20 mbps"
+tipo_de_red="4G"
+VPN="false"
+direccion_ip="192.168.1.33"
+direccion_mac="04:1:67:62:d3:14"
+puerta_de_enlace="192.168.1.1"
+velocidad_del_vinculo="72 mbps"
+frecuencia="2.4 GHz"
+tipo_de_seguridad="WPA/WPA2 psk"
+potencia_de_señal="excelente"
+programa="python"
+ruta="chiclayo lamabayeque"
+marca_de_movil="xiaomi"
+modelo="miA1"
+numero_de_telefono="224468"
+curso_1="basica 1"
+curso_2="analisis matematico"
+curso_3="tecnicas de estudio"
+curso_4="programacion 1"
+playa="pimentel"
+plato="arroz con mariscos"
+fecha_y_ubigeo="130101"
+dni_de_la_madre="16463165"
+apodo="kaiser"
+videojuego2="free fire"
+nota="10"
+aplicacion="spotify"
+nivel_de_bateria="80%"
+empresa="ENSA"
+empresa1="SEDAPAL"
+id="811319614"
+team="alfa"
+ensamblado="china"
+nombre_de_deporte="natacion"
+pasatiempo="videojuegos"
+horas_totales="96 horas academicas"
+voltaje="220 v"
+nombre_del_supermecado="plaza vea"
+nombre_del_centro_comercial="real plaza"
+color_de_cabello="negro"
+nombre_de_amigo="carlos"
+tipo_de_examen_de_admision="ordinario"
+ingreso="2019 II"
+fecha_de_egreso="2017"
+memoria_ram="8gb"
+cantidad_de_bits="32 bits"
+tipo_de_procesador="core i3"
+tarjeta_de_video="nvidia"
+nombre_del_banco="bcp"
+color_favorito="azul"
+facultad="facfym"
+nombre_de_la_carrera="ingeneria electronica"
+duracion="10 ciclos"
+novia="yohana"
+marca_de_lavadora="samsung"
+empresa_de_gas_natural="quavi"
+correo_institucional="rguerrero@unprg.edu"
+numero_de_trabajos="3"
+serie_favorita="la casa de papel"
+fecha_de_nacimiento="30/07/2001"
+metros_cuadrados="100m^2"
+tipo_de_entrada="tipo c"
+esta_prendido="true"
+tiene_carga="true"
+print("mi nombre es:", nombre_del_alumno +",mi n° de dni es:", numero_dni_del_alumno + "mi direccion es:", direccion)
+print("la fecha q deposite fue el:",fecha_de_deposito +"con el n° de operacion:", n_de_operacion + "la hora en que hice esa operacion es:", hora_de_operacion)
+print("hay un agente bcp en la:", agente_bcp + " ahi le deposite a mi amigo:", nombre + "el monto de:", monto_recibido)
+print("la categoria vehicular es:", categoria_vehicular + " y su clase es:", clase_vehicular + " el n° de licencia es:", n_de_licencia)
+print("la fecha de expedicion es:", fecha_de_expedicion + " y la de revalidacion es:", fecha_de_revalidacion,"la fecha de nacimiento del conductor es:" ,fecha_de_nacimiento_del_condcutor)
+print("el conductor es donante:", donate_de_organos + ",su grupo sanguinio es:", grupo_sanguinio + "y su numero primigenio", n_primigenio )
+print("el ruc de mi primo es:", ruc + ", su sery es", sery + " su clienta es:", cliente)
+print("ayer compre:", nombre_del_producto + ", me costo:", importe + " y el numero de la boleta es:",n_de_boleta)
+print("la direccion de la tienda de mi amiga es:", direccion_de_la_tienda + " la protietaria de aquella tienda es:", propietario + " el numero de recibo q ella proporciono al compar es:",n_de_recibo)
+print("la conexion de energia electrica es:", tipo_de_conexion + ",el consumo mensual equivale a:", consumo + "y mi potencia q contrate es de:", potencia_contratada)
+print("el inicio del contrato con:",nombre_de_la_empresa+" es:",inicio_de_contrato + " y culmina", termino_de_contrato)
+print("el codigo de reclamo de:",nombre_del_usuario+"con el dni:",DNI_del_cliente+" es:",codigo_de_consultas)
+print("el costo del alumbrado publico es:",alumbrado_publico+",ademas el I.G.V tiene un costo de:",IGV+" con un total a pagar de: ",total_a_pagar)
+print("la pagina web de:", nombre_de_la_empresa+ ",es:",pagina_web + " ahi adquiri un descuento de:",descuento_fise)
+print("el modulo del vector a es:",modulo + ",esta ubicado en el",direccion_y_sentido + ",eso enseño ", autor_de_introducion_al_analisis_matematico + " en su libro")
+print("mi codigo de estudiante es:",codigo_de_estudiante+" pague un importe de matricula ",importe_de_matricula+ "y en total me salio:", total)
+print("la pagina web de :", nombre_de_la_universidad +" y su pagina web es",pagina_web_de_la_unprg + " y  mi correo es:", correo_institucional)
+print("en mi primer ciclo llevo estos cursos:",curso_1,curso_2,curso_3+" y tambien",curso_4 )
+print("mi plan contratado es de:", plan_contratado + " con la red:", tipo_de_red+" que utiliza vpn",VPN)
+print("mi mejor amigo se llama:",nombre_de_amigo +" el color de su pelo es:",color_de_cabello +" y su ojos son de color:", color_de_ojos)
+print("mi novia es:",novia + " su color favorito es el ",color_favorito+ " y le gusta jugar",videojuego2)
+print("mi movil es marca",marca_de_movil +" ,el imei es:",imei +" y el modelo es",modelo)
+print("yo estudio",nombre_de_la_carrera +" q consta de",duracion + " en la faculta de",facultad)
+print("mi pc tiene",memoria_ram+"de ram,tambien tiene una tarjeta de video marca:",tarjeta_de_video+" y el procesador es un",tipo_de_procesador)
+print("el año q sali del colegio es:",fecha_de_egreso+" ingrese a la unprg en la modalidad de",tipo_de_examen_de_admision+"ingrese en el examen ",ingreso)
+print("mi serie favorita es",serie_favorita+" y mi hoobi es",pasatiempo)
+print("yo vivo en ",provincia+" departamento de:,",departamento+" y mi numero de telefono es:", numero_de_telefono)
+print("yo naci el ",fecha_de_nacimiento+" mi deporte favorito es:",nombre_de_deporte+" , mi plato favorito es:",plato)
+print("mi casa mide:",metros_cuadrados+" ahi tengo mi lavadora marca",marca_de_lavadora+" que utiliza :",voltaje)
+print("en la distrito de:",distrito+" ahi estan q instlan gas natural de :",empresa_de_gas_natural+" q fue ensamblado en",ensamblado)
+print("la bateria de mi celular esta al",nivel_de_bateria+",esta prendido",esta_prendido+" y  cargado", tiene_carga )
+print("en la farmancia:",tienda+" utilizan la moneda de ",tipo_de_moneda+" ")
+print("las empresas de servicios son", empresa+" y",empresa1+" que tien encargado las provincias de",ruta)
+print("el auto de mi amigo tiene",n_de_ruedas+" es de color:",color_del_vehiculo+" su placa es ",n_de_placa)
+print("mi cancion favorita es",cancion+" se escucha en todo el",pais+" ")
+print("yo juego",videojuego2+" mi apodo es:",apodo+" mi id es:",id)
+print("El programga q utilizo en la pc es: ",programa+" el profesor de :",nombre_del_curso+" nos enseño a usarlo"+" y tamben comprimir archvos en",tipo_de_archivo)
+print("mi direccion ip es:",direccion_ip+" y la mac es:",direccion_mac+" la frecuencia es:", frecuencia)
+print("yo tengo en la semana:",horas_totales+" con mi team:",team+" nuestro emoji es", emoji)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
